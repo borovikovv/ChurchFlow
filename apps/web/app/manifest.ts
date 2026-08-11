015b9cc76924a303d3b89f2602db0478a877f1c5
Oleksii Borovykov <borovikovvaleksey@gmail.com>
2026-08-11 12:11:03 +0300

    fix manifest file

--- apps/web/app/manifest.ts
+++ apps/web/app/manifest.ts
@@ -14,11 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icons/pwa-192x192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'any',
       },
       {
         src: '/icons/favicon.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'any',
       },
       {
         src: '/icons/pwa-maskable-512x512.png',
