996e08c48dceb14ad21a310914b9e065e0cffc35
Oleksii Borovykov <borovikovvaleksey@gmail.com>
2026-08-24 16:09:19 +0300

    fix types

--- apps/web/src/shared/index.ts
+++ apps/web/src/shared/index.ts
@@ -1,3 +1,4 @@
 export * from '../../../../packages/shared/src/constants';
+export * from '../../../../packages/shared/src/locale';
 export * from '../../../../packages/shared/src/schemas';
 export * from '../../../../packages/shared/src/types';
